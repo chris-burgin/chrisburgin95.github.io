821fe99a5ba612fbf44d6d0605c74c2cb8a29978
ChrisBurgin <chrisburgin95@gmail.com>
2016-01-06 14:08:25 -0500

    Minor Style Changes

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+//Package Imports
+var pkg = require('./package.json'),
+    gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano');
+
+//run 'gulp css'
+gulp.task('css', function() {
+    return gulp.src('_site/css/main.css')
+    .pipe(autoprefixer({
+        browsers: ['> 1%', 'IE 6', 'IE 7', 'IE 8', 'IE 9', 'IE 10'],
+        cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('_site/css'));
+});
+
+// run 'gulp watch'
+gulp.task('watch', function() {
+    gulp.watch('_site/css/main.css', ['css']);
+});
+
+// Default gulp task that runs when you 'gulp'
+gulp.task('default', ['css']);
