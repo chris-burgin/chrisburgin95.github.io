356210d3d5dc8443a88f3a1ee24ff19d605ced61
ChrisBurgin <chrisburgin95@gmail.com>
2016-03-01 23:16:49 -0500

    Major Redesign

--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -3,14 +3,15 @@ var pkg = require('./package.json'),
     gulp = require('gulp'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
+    myth = require('gulp-myth'),
     cssnano = require('gulp-cssnano');
 
 //run 'gulp css'
 gulp.task('css', function() {
     return gulp.src('_site/css/main.css')
+    .pipe(myth())
     .pipe(autoprefixer({
-        browsers: ['> 1%', 'IE 6', 'IE 7', 'IE 8', 'IE 9', 'IE 10'],
-        cascade: false
+        browsers: ['> 1%'],
     }))
     .pipe(cssnano())
     .pipe(rename({suffix: '.min'}))
